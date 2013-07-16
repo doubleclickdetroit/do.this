ef0c52a377c5a13ce578c47d699d4e915d9921d2
Brad Chase <bosco87@gmail.com>
2013-07-16 05:04:20 -0400

    more request specs

--- spec/requests/entities_spec.rb
+++ spec/requests/entities_spec.rb
@@ -26,25 +26,33 @@ describe EntitiesController do
       end
     end
 
-    describe "POST /entities" do
-      let(:entity) { entity = build(:entity).attributes }
+    describe "PUT/PATCH /entities" do
+      it "updates an existing Entity" do
+        entity = create(:entity)
         
-      it "creates a new Entity" do
-        expect {
-          post entities_path, {format: :json, entity: entity}
-        }.to change(Entity, :count).by(1)
-        # response.status.should be(201)
-        # JSON.parse(response.body)['title'].should eq('foo')
-      end
+        title = entity.title + "foo"
+        entity.title.should_not eq(title)
 
-      it "creates a new Entity" do
-        pending 'broken on time'
-        post entities_path, {format: :json, entity: entity}
-        e = Entity.last
-        e.title.should eq(entity['title'])
-        e.note.should  eq(entity['note'])
-        e.time.should  eq(entity['time'])
-        e.date.should  eq(entity['date'])
+        put entity_path(entity), {format: :json, entity: {title: title}}
+        entity.reload
+        entity.title.should eq(title)
+
+        title = entity.title + "bar"
+        entity.title.should_not eq(title)
+
+        patch entity_path(entity), {format: :json, entity: {title: title}}
+        entity.reload
+        entity.title.should eq(title)
+      end
+    end
+
+    describe "DELETE /entities" do
+      it "destroys an existing Entity" do
+        entity = create(:entity)
+
+        expect {
+          delete entity_path(entity), {format: :json}
+        }.to change(Entity, :count).by(-1)
       end
     end
   end
