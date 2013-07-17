dc2a971b7e6b7e4ddc97116370924f68b15c407e
Brad Chase <bosco87@gmail.com>
2013-07-17 03:31:25 -0400

    add name to users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    sequence(:name)  {|n| "User ##{n}"}
     sequence(:email) {|n| "user#{n}@example.com"}
     password 'asdfasdf'
     password_confirmation 'asdfasdf'
