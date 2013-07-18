b72aba5a1dd3a4a2c0d929190153e253430b780b
Brad Chase <bosco87@gmail.com>
2013-07-18 01:35:30 -0400

    Entities have many Users

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,6 +2,7 @@ class Entity < ActiveRecord::Base
   belongs_to :user
   has_many :tags, as: :taggable
   has_many :relationships, as: :relatable
+  has_many :users, through: :relationships
 
   def name
     title
