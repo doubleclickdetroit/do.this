90e4caa9d28ec18280a6d96df036f38857ee6dae
Brad Chase <bosco87@gmail.com>
2013-07-29 02:44:58 -0400

    add user_id to stories; stub story spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130729063857) do
+ActiveRecord::Schema.define(version: 20130729064232) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -53,7 +53,10 @@ ActiveRecord::Schema.define(version: 20130729063857) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "stories", ["user_id"], name: "index_stories_on_user_id", using: :btree
 
   create_table "tags", force: true do |t|
     t.string   "name"
